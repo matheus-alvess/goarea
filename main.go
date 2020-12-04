313f8c38990e02875ea814d4c1c89a2a7a02eb7e
Matheus Alves <matheus.alves@mercadolivre.com>
2020-12-03 23:52:54 -0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,22 @@
+package area // por padrão o nome do pacote é o mesmo nome da pasta em que esta
+// contido
+import "math"
+
+// PI é uma proporcao numerica
+const PI = math.Pi
+
+// Circ calcula a area da circunferencia
+func Circ(raio float64) float64 {
+	return math.Pow(raio, 2) * PI
+}
+
+// Rect calcula a area de um retangulo
+func Rect(base, altura float64) float64 {
+	return base * altura
+}
+
+// não é visivel
+func _TrianguloEq(base, altura float64) float64 {
+	return (base * altura) / 2
+}
+
